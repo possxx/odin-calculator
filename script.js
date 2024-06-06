4863ca29fef7a92b3340c69347df591b19de6448
possxx <168581964+possxx@users.noreply.github.com>
2024-06-06 17:21:27 +0200

    Create basic calculator functions

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(a, b, operator) {
+    if (operator == "+") {
+        return add(a, b);
+    } else if (operator == "-") {
+        return subtract(a, b);
+    } else if (operator == "*") {
+        return multiply(a, b);
+    } else if (operator =="/") {
+        return divide(a, b);
+    }
+};
+
